4eb29f3e4f72092fd3cb5e193f4f6586f2945b84
Crowdbotics <engineering@crowdbotics.com>
2023-05-12 10:09:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-moon-dev-82649",
+  url: "https://holy-moon-dev-82649.botics.co",
+  api: "https://holy-moon-dev-82649.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
